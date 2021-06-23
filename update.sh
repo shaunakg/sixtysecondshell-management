16a0ad03dd743adce3581e4925b6420c33c6a1a4
Shaunak G <gadkari.shaunak@gmail.com>
2021-06-23 12:05:34 +1000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+
+echo Pulling image...
+docker pull docker.pkg.github.com/shaunakg/sixtysecondshell/sixtysecondshell:latest
+
+echo Stopping current container...
+docker kill sixtyseconds-container
+docker rm sixtyseconds-container
+
+echo Starting new container...
+docker run --privileged -d -v /var/tmp:/usr/src/app/__code_store -v /var/run/docker.sock:/var/run/docker.sock -p 80:80 --name "sixtyseconds-container" docker.pkg.github.com/shaunakg/sixtysecondshell/sixtysecondshell:latest
+echo Done.
