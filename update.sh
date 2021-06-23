2517a1e34957bd0d8a2d115db1348af90c482bec
Shaunak G <gadkari.shaunak@gmail.com>
2021-06-23 21:25:54 +1000

    fixed bug where script would pull image then delete it

--- update.sh
+++ update.sh
@@ -6,9 +6,10 @@ echo Stopping current container...
 docker kill sixtyseconds-container
 docker rm sixtyseconds-container
 
-echo Pruning all images to save disk space...
-docker system prune -af
-
 echo Starting new container...
 docker run --privileged -d -v /var/tmp:/usr/src/app/__code_store -v /var/run/docker.sock:/var/run/docker.sock -p 80:80 --name "sixtyseconds-container" docker.pkg.github.com/shaunakg/sixtysecondshell/sixtysecondshell:latest
+
+echo Pruning all unused images to save disk space...
+docker system prune -af
+
 echo Done.
